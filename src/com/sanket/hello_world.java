6d8ef07a64000bfeb056392d306ccbb21ab47239
https://github.com/kasanket64-prog <sanketkalbhor385@gmail.com>
2026-08-25 23:39:02 +0530

    Initial commit - setup project

---
+++ src/com/sanket/hello_world.java
@@ -0,0 +1,11 @@
+package com.sanket;
+
+public class hello_world {
+    public static void   main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+//This is my first code in java
+
+C: /user/sanket/ideaprojects/java_full_practice
