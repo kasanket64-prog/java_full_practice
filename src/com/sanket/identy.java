9af3484ff7439f4d707ab6e51138e3ea88d73cf3
https://github.com/kasanket64-prog <kasanket64@gmail.com>
2026-08-27 02:12:40 +0530

    done

---
+++ src/com/sanket/identy.java
@@ -0,0 +1,13 @@
+package com.sanket;
+
+public class identy {
+    static void main (String[] args){
+        System.out.println("-------Identity---------");
+        System.out.println("Name  = Sanket Santosh Kalbhor");
+        System.out.println("State = Maharashtra");
+        System.out.println("City  = Pune");
+        System.out.println("Hobby = Learning New Things");
+
+
+    }
+}
