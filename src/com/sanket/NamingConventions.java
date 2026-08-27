b8a45d3091676fd9ade91c08eee7fc4790723a2c
https://github.com/kasanket64-prog <kasanket64@gmail.com>
2026-08-28 00:25:34 +0530

    done

---
+++ src/com/sanket/NamingConventions.java
@@ -0,0 +1,22 @@
+package com.sanket;
+
+public class NamingConventions
+        /*  n is capital rest all letters are
+small letters,then second word first letter is capital and rest are small case */
+{
+    public static void main(String[] args)
+        /*here in camelCaseConvention the first word
+    in starting is small and rest word are same as pascal convention */
+    {
+        System.out.println("-------Types of conventions---------");
+        System.out.println("PascalConvention");
+        /* [ 1]PascalCase (UpperCamelCase) → Each word starts with a capital letter.
+
+           [2]Should be nouns (representing entities).  */
+
+        System.out.println("camelCaseConvention");
+        /*  [1]camelCase (lowerCamelCase) → First word lowercase, subsequent words capitalized.
+
+             [2]Should be verbs (representing actions).  */
+    }
+}
