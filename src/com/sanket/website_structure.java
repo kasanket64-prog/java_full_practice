d7402f9f7bd9ac9bd24462fc76b8b60feda81f11
https://github.com/kasanket64-prog <kasanket64@gmail.com>
2026-08-27 10:35:25 +0530

    done

---
+++ src/com/sanket/website_structure.java
@@ -0,0 +1,10 @@
+package com.sanket;
+
+public class website_structure {
+    static void main (String[] args){
+        System.out.println("-------- website_structure--------");
+        System.out.println("frontend");
+        System.out.println("backend");
+        System.out.println("database");
+        System.out.println("host server");
+}}
