9ba20cdd7045a04fae3b3e1a78acb14908822770
https://github.com/kasanket64-prog <kasanket64@gmail.com>
2026-08-28 02:38:54 +0530

    done

---
+++ src/com/sanket/substraction.java
@@ -0,0 +1,12 @@
+package com.sanket;
+
+public class substraction {
+    public static void main(String[] args) {
+        int a = 121, b = 122, c;
+        c = b - a;
+        System.out.println("------- substraction of two numbers-------- ");
+        System.out.println("where a=121,b=122,c=?");
+        System.out.println("find c=?");
+        System.out.println("c =" + c);
+    }
+}
