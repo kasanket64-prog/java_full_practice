dfe1471554cf637d1d6f8a2e8c53a1bd8042236d
https://github.com/kasanket64-prog <kasanket64@gmail.com>
2026-08-29 21:26:12 +0530

    done

---
+++ src/com/sanket/average.java
@@ -0,0 +1,18 @@
+package com.sanket;
+
+public class average
+ {
+    public static void main(String[] args) {
+        double num1 = 10;
+        double num2 = 20;
+        double num3 = 30;
+        double num4 = 40;
+        double num5 = 50;
+
+        double sum = num1 + num2 + num3 + num4 + num5;
+        double average = sum / 5;
+
+        System.out.println("The average is: " + average);
+    }
+}
+
