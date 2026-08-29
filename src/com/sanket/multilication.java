db7dbe39f62f3adf0fec137a8dd5b179cb468f9e
https://github.com/kasanket64-prog <kasanket64@gmail.com>
2026-08-29 21:18:44 +0530

    done

---
+++ src/com/sanket/multilication.java
@@ -0,0 +1,14 @@
+package com.sanket;
+
+public class multilication {
+
+    public static void main(String[] args) {
+        int a = 121, b = 122, c;
+        c = b * a;
+        System.out.println("-------  multilication  of two numbers-------- ");
+            System.out.println("where a=121,b=122,c=?");
+            System.out.println("find c=?");
+            System.out.println("c =" + c);
+        }
+    }
+
