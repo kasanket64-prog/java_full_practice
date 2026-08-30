7aff07ec376244b150a9d3aa0259fb0ba6183ff2
https://github.com/kasanket64-prog <kasanket64@gmail.com>
2026-08-30 23:33:47 +0530

    done

---
+++ src/com/sanket/Simplecalculator.java
@@ -0,0 +1,21 @@
+package com.sanket;
+
+public class Simplecalculator  {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        int quot = a / b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + diff);
+        System.out.println("Product = " + prod);
+        System.out.println("Quotient = " + quot);
+    }
+}
+
