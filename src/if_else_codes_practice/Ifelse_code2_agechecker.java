cda831207d0d923980977d0401db45b50551e77d
https://github.com/kasanket64-prog <kasanket64@gmail.com>
2026-08-31 01:02:14 +0530

    done

---
+++ src/if_else_codes_practice/Ifelse_code2_agechecker.java
@@ -0,0 +1,15 @@
+package if_else_codes_practice;
+
+public class Ifelse_code2_agechecker {
+    public static void main(String[] args) {
+        int age = 19;
+
+        if (age <= 18) {
+            System.out.println("You are small");
+        } else {
+            System.out.println("You are now Big ");
+        }
+
+
+    }
+}
